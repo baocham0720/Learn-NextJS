cea74bfafa4122b3786941e45806b352f55e88bb
baotram <hbaotram01@gmail.com>
2024-04-18 09:01:46 +0700

    next-auth

--- nextjs-app-router/src/app/login/page.tsx
+++ nextjs-app-router/src/app/login/page.tsx
@@ -1,22 +1,31 @@
 import {Metadata} from 'next'
-
 export const metadata: Metadata = {
   title: 'Login',
   description: 'Login description'
 }
 
-import RedirectButton from "@/components/ui/RedirectButton"
+//import RedirectButton from "@/components/ui/RedirectButton"
 
+import LoginForm from "@/components/ui/LoginForm"
+import { getCsrfToken } from "next-auth/react"
 /**
- * 
- * Được hiểu ngầm định là Server component
+ * Tính năng NextAuth liên quan đến các file
+ * app/layout.tsx
+ * app/login/page.tsx
+ * app/api/auth/[...nextauth]/route.tsx
+ * app/customers/layout.tsx
+ * ---------------------
+ * Follow login
+ * LoginForm --> SignIn --> app/api/auth/[...nextauth]/route.tsx --> goi ham authorize() 
+ * --> login thanh cong thi return user
+ * --> luu thong tin user vao session
  */
-const LoginPage = () => {
- 
+const LoginPage = async () => {
+  const csrfToken = await getCsrfToken()
   return (
     <div>
-        <h1>Login Pages</h1>
-        <RedirectButton label="Go To Home" />
+         <LoginForm csrfToken={csrfToken} />
+        {/*<RedirectButton label="Go To Home" />*/}
     </div>
   )
 }
@@ -24,3 +33,6 @@ const LoginPage = () => {
 export default LoginPage
 
 
+//"email": "john@mail.com",
+//"password": "changeme",
+//theo fakeapi
